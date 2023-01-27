5f488d0aaa181bda743b37173d5b0a69996c931f
FullCycleConceptaria <fullcycleconceptaria@outlook.com>
2023-01-27 15:58:12 +0100

    footer start

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,25 @@
 import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
-    <div className={styles.footer}></div>
-  )
+    <div className={styles.footer}>
+      <div className={styles.footer_text}>
+        <p>Informations et contact</p>
+        <a href='http://' target='_blank'>
+          info@merciki.be
+        </a>
+        <Link>Formulaire de contact</Link>
+        <p>En savoir plus</p>
+        <Link>Questions fréquentes</Link>
+        <Link>Merciki dans la presse</Link>
+        <Link>Conditions d'utilisation</Link>
+        <Link>Règlement relatif à la vie privée</Link>
+        <p>Mon Merciki</p>
+        <Link>S'inscrire</Link>
+        <Link>Se connecter</Link>
+      </div>
+      <div className={styles.footer_icons}></div>
+    </div>
+  );
 }
