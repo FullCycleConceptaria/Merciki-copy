514ef0ff555004f1fa224da2fd590000d1c1e5ec
NiteChill <puissantachille@gmail.com>
2023-02-03 22:25:25 +0100

    How does it work done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,13 @@
 import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import Reasons from '../../components/Reasons/Reasons';
+import HowDoesItWork from '../../components/HowDoesItWork/HowDoesItWork';
 
 export default function Home() {
   return (
     <>
       <HeroHeader />
       <Reasons />
+      <HowDoesItWork />
     </>
   );
 }
