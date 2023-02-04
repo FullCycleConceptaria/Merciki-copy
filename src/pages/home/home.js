7de1e30967384b97640f67644a65be074b4957d4
DradarkTV <dradarkx@gmail.com>
2023-02-04 15:25:39 +0100

    WhatCanIDo Finalisation

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,13 @@
 import HeroHeader from '../../components/HeroHeader/HeroHeader';
 import Reasons from '../../components/Reasons/Reasons';
+import WhatCanIDoHere from '../../components/WhatCanIDoHere/WhatCanIDoHere';
 
 export default function Home() {
   return (
     <>
       <HeroHeader />
       <Reasons />
+      <WhatCanIDoHere />
     </>
   );
 }
