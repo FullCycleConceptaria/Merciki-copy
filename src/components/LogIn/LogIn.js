57f5c9265004575d227d6763a6f938bc4b4ef8b2
FullCycle <sjbonjean@gmail.com>
2023-02-11 16:59:34 +0100

    test fix gestion scroll modale smartphone

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,23 +1,29 @@
-import { useEffect } from 'react';
-import styles from './LogIn.module.scss';
+import { useEffect } from "react";
+import styles from "./LogIn.module.scss";
 
 export default function LogIn({ modale }) {
   useEffect(() => {
     if (modale) {
-      window.addEventListener('wheel', prevent,{passive:false});
-      window.addEventListener('scroll', prevent,{passive:false});
+      window.addEventListener("wheel", prevent, { passive: false });
+      window.addEventListener("scroll", prevent, { passive: false });
+      window.addEventListener("touchstart", prevent, { passive: false });
+      window.addEventListener("touchmove", prevent, { passive: false });
     } else {
-      window.removeEventListener('wheel', prevent);
-      window.removeEventListener('scroll', prevent);
+      window.removeEventListener("wheel", prevent);
+      window.removeEventListener("scroll", prevent);
+      window.removeEventListener("touchstart", prevent);
+      window.removeEventListener("touchmove", prevent);
     }
     return () => {
-      window.removeEventListener('wheel', prevent);
-      window.removeEventListener('scroll', prevent);
+      window.removeEventListener("wheel", prevent);
+      window.removeEventListener("scroll", prevent);
+      window.removeEventListener("touchstart", prevent);
+      window.removeEventListener("touchmove", prevent);
     };
   }, [modale]);
 
   function prevent(e) {
-    console.log('prevent');
+    console.log("prevent");
     e.preventDefault();
   }
 
@@ -26,8 +32,9 @@ export default function LogIn({ modale }) {
       className={styles.login}
       style={
         modale
-          ? { transform: 'translateY(-50%)' }
-          : { transform: 'translateY(100%)' }
-      }></div>
+          ? { transform: "translateY(-50%)" }
+          : { transform: "translateY(100%)" }
+      }
+    ></div>
   );
 }
